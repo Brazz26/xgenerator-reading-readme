8cbdac95f589c82e068652314d56d70929da21c7
Brazz26 <147454142+Brazz26@users.noreply.github.com>
2024-01-24 14:18:09 -0500

    created function to generate and connect readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,29 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+       type: 'input',
+       name: 'title',
+       message: 'What is the title of your application?' 
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(path.join(fileName),data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers)=>{
+        console.log(answers.title);
+        writeToFile('readme.md', generateMarkdown({...answers}))
+    })
+}
 
 // Function call to initialize app
 init();
