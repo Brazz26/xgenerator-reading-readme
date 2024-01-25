991333d728512f0d5be0295c5457a60811a536f3
Brazz26 <147454142+Brazz26@users.noreply.github.com>
2024-01-24 21:33:36 -0500

    added new badge and deleted BadgeMaker

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,10 @@
-// TODO: Include packages needed for this application
+// TODO: Packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
-// TODO: Create an array of questions for user input
+
+// TODO: Array of questions for user input
 const questions = [
     {
        type: 'input',
@@ -20,17 +21,23 @@ const questions = [
         name: 'license',
         message: 'what license would you like?',
         choices: [
-            'MIT', 'APACHE2.0', 'GPL3.0', 'BSD3', 'None'
+            'MIT',
+            'APACHE2.0', 
+            'GPL3.0',
+            'BSD1',
+            'BSD2', 
+            'BSD3',
+            'None'
         ]
     }
 ];
 
-// TODO: Create a function to write README file
+// TODO: Function to write README file
 function writeToFile(fileName, data) {
     fs.writeFileSync(path.join(fileName),data)
 }
 
-// TODO: Create a function to initialize app
+// TODO: Function to initialize app
 function init() {
     inquirer.prompt(questions).then((answers)=>{
         console.log(answers.title);
