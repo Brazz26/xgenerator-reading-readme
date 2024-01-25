6a0af32c0be4850b488a7810694bdd1f0c7953e5
Brazz26 <147454142+Brazz26@users.noreply.github.com>
2024-01-24 22:08:39 -0500

    housekeeping

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== 'None') {
@@ -7,7 +7,7 @@ function renderLicenseBadge(license) {
   return '';
 }
 
-// TODO: Create a function that returns the license link
+// TODO: Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== 'None') {
@@ -16,7 +16,7 @@ function renderLicenseLink(license) {
   return '';
 }
 
-// TODO: Create a function that returns the license section of README
+// TODO: Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== 'None') {
@@ -27,7 +27,7 @@ function renderLicenseSection(license) {
   return '';
 }
 
-// TODO: Create a function to generate markdown for README
+// TODO: Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
@@ -37,8 +37,7 @@ function generateMarkdown(data) {
   * [Usage](#usage)
   * [Credits](#credits)
   ${renderLicenseLink(data.license)}
-  * []()
-  * []() 
+  
    
 
 ## Description
